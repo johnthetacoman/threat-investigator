2f4bf807295a3542009af8d7e4db1b876b656ed8
daneedore <daneedore@gmail.com>
2020-05-19 21:40:17 -0700

    Implemented command line args for ip

--- threat_investigator.py
+++ threat_investigator.py
@@ -1,35 +1,32 @@
+import sys 
 import webbrowser
 
-def get_user_input(): 
+def investigate_ip_address(ip_address):
     """
-    Obtains a(an) IP address/domain/URL and returns it as a string
-
+    Determine if the IP address is a threat by opening investigation links.
+    Args:
+        ip_address(required): the IP address that will be investigated.
     """
-    userInput = input("Please enter the IP address/domain/URL to investigate: ")
+    #Create the proper links to open in the browser for threat investigation:
+    ip_address_links = ["https://exchange.xforce.ibmcloud.com/ip/" + ip_address,
+             "https://www.abuseipdb.com/check/" + ip_address + "/", 
+             "https://talosintelligence.com/reputation_center/lookup?search=" + ip_address + "/", 
+             "https://www.virustotal.com/gui/ip-address/" + ip_address + "/detection" ,
+             "https://www.malwareurl.com/listing-urls.php",
+             "https://www.brightcloud.com/tools/url-ip-lookup.php", 
+             "https://www.shodan.io/search?query=" + ip_address + "/",
+             "https://fortiguard.com/webfilter?q=" + ip_address + "&version=8",
+             "https://www.ipvoid.com/",
+             "https://centralops.net/co/DomainDossier.aspx",
+             "https://viewdns.info/reverseip/?host=" + ip_address + "&t=1"]
 
-    return userInput   
+    #Determine the severity of the ip address using the links' results:
+    for ip_address_link in ip_address_links:
+        webbrowser.open_new_tab(ip_address_link)
 
-def open_links(links):
-    """
-    Opens links in the web browser
-
-    """
-    for link in links:
-        webbrowser.open_new_tab(link)
-
-# Get the user's requested IP address/Domain/ URL to investigate:
-userInput = get_user_input()
-
-links = ["https://exchange.xforce.ibmcloud.com/ip/" + userInput,
-         "https://www.abuseipdb.com/check/" + userInput + "/", 
-         "https://talosintelligence.com/reputation_center/lookup?search=" + userInput + "/", 
-         "https://www.virustotal.com/gui/ip-address/" + userInput + "/detection" ,
-         "https://www.malwareurl.com/listing-urls.php",
-         "https://www.brightcloud.com/tools/url-ip-lookup.php", 
-         "https://www.shodan.io/search?query=" + userInput + "/",
-         "https://fortiguard.com/webfilter?q=" + userInput + "&version=8",
-         "https://www.ipvoid.com/",
-         "https://centralops.net/co/DomainDossier.aspx",
-         "https://viewdns.info/reverseip/?host=" + userInput + "&t=1"]
-
-open_links(links)
+if __name__ == "__main__":
+    #Investigate the correct ip address/domain/URL based on argument 1:
+    if (sys.argv[1] == "ip"):
+        investigate_ip_address(sys.argv[2])
+    else: 
+        print("Please insert an IP address.")
